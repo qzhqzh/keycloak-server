6105276db346827eae2e6391fc2a3b983d3988e3
qzhqzh <qin28630707@gmail.com>
2026-02-08 20:05:20 +0800

    feat: quickly init

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Starting Keycloak server...
+docker compose up -d
+
+echo Keycloak is starting...
+echo Please wait a few seconds for the service to be ready.
+echo.
+echo Access Keycloak Admin Console at: http://localhost:8080/admin
+echo Username: admin
+echo Password: admin
+echo.
+echo To view logs: docker compose logs -f
+echo To stop: docker compose down
+pause
